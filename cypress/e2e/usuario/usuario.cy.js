3f20fdd413a3c2ad02b0b331850749bfc73228d5
Eduardo Moises <eduardomoises@MMM00928BRZ.local>
2025-01-14 13:49:20 -0300

    Implemtacao dos testes de usuario

--- cypress/e2e/usuario/usuario.cy.js
+++ cypress/e2e/usuario/usuario.cy.js
@@ -1,21 +1,37 @@
-describe('Testes de API', () => {
-    it('Deve validar o retorno do endpoint GET /usuarios', () => {
-      cy.request('GET', '/usuarios').then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.have.property('usuarios');
-        // Valide mais campos da resposta conforme necessário
-      });
-    });
-  
-    it('Deve criar um novo usuário via POST /usuarios', () => {
-      cy.request('POST', '/usuarios', {
-        nome: 'João Silva',
-        email: 'joao@exemplo.com',
-        senha: 'senha123',
-      }).then((response) => {
-        expect(response.status).to.eq(201);
-        expect(response.body).to.have.property('id');
-      });
+describe('Testes de API endpojt usuario ', () => {
+
+  // Carrega o arquivo JSON com o faker
+  before(() => {
+    cy.exec('node generateData.js').its('code').should('eq', 0);  // Verifica se o comando foi bem-sucedido
+    cy.fixture('data').as('testData'); // Lê o arquivo 'data.json' na pasta fixtures
+  });
+
+  it('Nao permite cadastrar  usuario com email existente na base', () => {
+    cy.postUsuario("Fulano da Silva").then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.have.property('message', 'Este email já está sendo usado');
+
     });
   });
-  
+
+  it('Deve cadastrar novo usuario como administrador', () => {
+    cy.postUsuarioValido("true").then((response) => {
+      expect(response.status).to.eq(201);
+      expect(response.body).to.have.property("message", "Cadastro realizado com sucesso");
+    });
+  });
+
+  it('Deve cadastrar novo usuario', () => {
+    cy.postUsuarioValido("false").then((response) => {
+      expect(response.status).to.eq(201);
+      expect(response.body).to.have.property("message", "Cadastro realizado com sucesso");
+    });
+  });
+
+    it('Deve listar usuários com sucesso', () => {
+      cy.getUsuarios().then((response) => {
+        expect(response.status).to.eq(200);
+      });
+    });
+
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,23 +1,10 @@
-//Token
-Cypress.Commands.add('loginUser', () => {
-    cy.api({
-        url: `${Cypress.env('API_URL')}/api/login`,
-        method: 'POST',
-        body: {
-            username: Cypress.env('USERNAME'),
-            password: Cypress.env('PASSWORD')
-        },
-        failOnStatusCode: false
-    }).then(response => { return response })
-})
-
-// Post usuario
+// Post usuario ja existente 
 Cypress.Commands.add('postUsuario', (nome) => {
     cy.fixture('Usuario/postUsuario').then((items) => {
         const specificItem = items.find(item => item.nome === nome);
         // Realiza  o cadastro do novo usuario
         cy.api({
-            url: `${Cypress.env('API_URL')}/api/Company`,
+            url: `${Cypress.env('API_URL')}/usuarios`,
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -29,3 +16,33 @@ Cypress.Commands.add('postUsuario', (nome) => {
         });
     });
 });
+// Post para criar novo usuario 
+Cypress.Commands.add('postUsuarioValido', (administrador) => {
+    cy.fixture('data').then((items) => {
+        const specificItem = items.find(item => item.administrador === administrador);
+        // Realiza  o cadastro do novo usuario
+        cy.api({
+            url: `${Cypress.env('API_URL')}/usuarios`,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: specificItem,
+            failOnStatusCode: false
+        }).then(response => {
+            return response;
+        });
+    });
+});
+// Get listar todos usuario 
+Cypress.Commands.add('getUsuarios', () => {
+        cy.api({
+            url: `${Cypress.env('API_URL')}/usuarios`,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(response => {
+            return response;
+        });
+    });
