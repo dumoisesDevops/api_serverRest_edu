28d63951be7f99763163169e33af78ae51b2adce
Eduardo Moises <eduardomoises@MMM00928BRZ.local>
2025-01-14 21:06:06 -0300

    Atualizar pipeline para  deixar o artefato mais dinamico

--- cypress/e2e/usuario/usuario.cy.js
+++ cypress/e2e/usuario/usuario.cy.js
@@ -43,7 +43,7 @@ describe('Testes de API endpojt usuario ', () => {
   });
 
   it('Deve excluir registro', () => {
-    cy.deleteUsuarioValido("7BcfUdEoChzUMgHE").then((response) => {
+    cy.deleteUsuarioValido("7fDqD6nV0bTzlGzM").then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body).to.have.property("message", "Registro excluído com sucesso")
 
